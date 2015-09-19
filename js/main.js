52e627f04b5b336f26208f82b49901c3442e9d0a
Matt Neill <matt.neill.work@gmail.com>
2015-09-19 14:34:46 -0400

    Panel update + carousel
    Using OwlCarousel for photo slider

--- js/main.js
+++ js/main.js
@@ -5,28 +5,48 @@
 	matt@poetretail.com
 */
 
-$(document).ready(function(e) {	
-	youtubeResize()
-	
-	$('#show-nav, #whiteover').click(function()
-	{
+$(document).ready(function(e) {
+	youtubeResize();
+	photosliderInit('.panel-media')
+
+	$('#show-nav, #whiteover').click(function() {
 		navSlideToggle()
 	});
-	
+
 });
 
 $(window).resize(function(){
 	youtubeResize()
 });
 
-function youtubeResize()
-{
-	$('#youtube-video').css('height', ($('#youtube-video').width())*.62);	//16:9 & padding	
+function youtubeResize() {
+	$('#youtube-video').css('height', ($('#youtube-video').width())*.62);	//16:9 & padding
 }
 
-function navSlideToggle()
-{
+function navSlideToggle() {
 	$('#nav-list').toggleClass('nav-slide');
 	$('#show-nav').toggleClass('nav-open');
-	$('#whiteover').fadeToggle(100);	
+	$('#whiteover').fadeToggle(100);
+}
+
+/*--------------- Slideshows ---------------*/
+var photoslider;
+var photosliderInit;
+photosliderInit = function(elem){
+
+	$(elem).owlCarousel({
+		autoPlay	: false,
+		pagination	: false,
+		lazyLoad	: true,
+		singleItem	: true,
+		rewindSpeed : 1000,
+		dragBeforeAnimFinish : false
+	});
+	photoslider = elem;
+	sliderData = $(elem).data('owlCarousel');
+}
+
+function sliderDestroy() {
+	photoslider = $('.slider').data('owlCarousel');
+	if (photoslider) photoslider.destroy();
 }
